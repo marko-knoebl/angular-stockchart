3255a940413de3ea290d103f689e40f509db52f5
Marko Knöbl <marko.kn@gmail.com>
2015-11-21 12:59:46 +0100

    make it possible to enter custom dates

--- controllers.js
+++ controllers.js
@@ -5,5 +5,16 @@
 var stockChartApp = angular.module('stockChartApp', []);
 
 stockChartApp.controller('StockChartCtrl', function($scope) {
+
   $scope.prices = [];
+
+  // default date range: 1 month
+  var date = new Date();
+  $scope.endDate = date.toISOString().slice(0,10);
+  date.setMonth(date.getMonth() - 1);
+  if (date.getMonth() === -1) {
+    date.setMonth(11);
+    date.setYear(date.getYear() - 1);
+  }
+  $scope.startDate = date.toISOString().slice(0,10);
 });
